05741113d4ac8b3a0724e42ace93eca75dd033b3
Faye Purdum <fayepurdum@gmail.com>
2013-03-31 18:04:11 -0700

    returning lat/long to browser

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,12 @@ class LocationsController < ApplicationController
           format.json { render json: @location }
       end
     end
+    def show
+      @location = Location.find(params[:id])
+
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @location }
+      end
+    end
 end
